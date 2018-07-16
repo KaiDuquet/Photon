c218749090c9598fa5827ccb30e088566813bc96
Kai Duquet <kaiduquet@gmail.com>
2018-07-16 01:17:58 -0400

    AST added and started parsing Declarations

---
+++ src/phast.py
@@ -0,0 +1,323 @@
+TYPESPEC_NAME = "TYPESPEC_NAME"
+TYPESPEC_FUNC = "TYPESPEC_FUNC"
+TYPESPEC_ARRAY = "TYPESPEC_ARRAY"
+TYPESPEC_PTR = "TYPESPEC_PTR"
+
+class Typespec(object):
+    """docstring for Typespec."""
+    def __init__(self, kind):
+        self.kind = kind
+
+class TypespecName(Typespec):
+    """docstring for TypespecName."""
+    def __init__(self, name):
+        super(TypespecName, self).__init__(TYPESPEC_NAME)
+        self.name = name
+
+class TypespecFunc(Typespec):
+    """docstring for TypespecFunc."""
+    def __init__(self, arg_types, ret_type):
+        super(TypespecFunc, self).__init__(TYPESPEC_FUNC)
+        self.arg_types = arg_types
+        self.ret_type = ret_type
+
+class TypespecArray(Typespec):
+    """docstring for TypespecArray."""
+    def __init__(self, elem_type, size):
+        super(TypespecArray, self).__init__(TYPESPEC_ARRAY)
+        self.elem_type = elem_type
+        self.size = size
+
+class TypespecPtr(Typespec):
+    """docstring for TypespecPtr."""
+    def __init__(self, elem_type):
+        super(TypespecPtr, self).__init__(TYPESPEC_PTR)
+        self.elem_type = elem_type
+
+
+DECL_FUNC = "DECL_FUNC"
+DECL_ENUM = "DECL_ENUM"
+DECL_STRUCT = "DECL_STRUCT"
+DECL_UNION = "DECL_UNION"
+DECL_VAR = "DECL_VAR"
+DECL_CONST = "DECL_CONST"
+DECL_TYPEDEF = "DECL_TYPEDEF"
+
+class Decl(object):
+    """docstring for Decl ."""
+    def __init__(self, kind, name):
+        self.kind = kind
+        self.name = name
+
+class FuncArg(object):
+    """docstring for FuncArg."""
+    def __init__(self, name, type_):
+        self.name = name
+        self.type_ = type_
+
+class DeclFunc(Decl):
+    """docstring for DeclFunc."""
+    def __init__(self, name, args, ret_type, block):
+        super(DeclFunc, self).__init__(DECL_FUNC, name)
+        self.args = args
+        self.ret_type = ret_type
+        self.block = block
+
+class EnumItem(object):
+    """docstring for EnumItem."""
+    def __init__(self, name, expr):
+        self.name = name
+        self.expr = expr
+
+class DeclEnum(Decl):
+    """docstring for DeclEnum."""
+    def __init__(self, name, items):
+        super(DeclEnum, self).__init__(DECL_ENUM)
+        self.items = items
+
+class AggregateField(object):
+    """docstring for AggregateField."""
+    def __init__(self, name, type_, data_decl):
+        if name is None and type_ is None:
+            self.data_decl = data_decl
+        else:
+            self.name = name
+            self.type_ = type_
+
+class DeclAggregate(Decl):
+    """docstring for DeclAggregate."""
+    def __init__(self, kind, name, fields):
+        assert(kind == DECL_STRUCT or kind == DECL_UNION)
+        super(DeclStruct, self).__init__(kind, name)
+        self.fields = fields
+
+class DeclVar(Decl):
+    """docstring for DeclVar."""
+    def __init__(self, name, type_, expr):
+        super(DeclVar, self).__init__(DECL_VAR, name)
+        self.type_ = type_
+        self.expr = expr
+
+class DeclConst(Decl):
+    """docstring for DeclConst."""
+    def __init__(self, name, expr):
+        super(DeclConst, self).__init__(DECL_CONST, name)
+        self.expr = expr
+
+class DeclTypedef(Decl):
+    """docstring for DeclTypedef."""
+    def __init__(self, name, _type):
+        super(DeclTypedef, self).__init__(DECL_TYPEDEF, name)
+        self.type_ = type_
+
+#############################################################
+
+STMT_DECL = "STMT_DECL"
+STMT_IF = "STMT_IF"
+STMT_WHILE = "STMT_WHILE"
+STMT_FOR = "STMT_FOR"
+STMT_DO_WHILE = "STMT_DO_WHILE"
+STMT_SWITCH = "STMT_SWITCH"
+STMT_RETURN = "STMT_RETURN"
+STMT_BREAK = "STMT_BREAK"
+STMT_CONTINUE = "STMT_CONTINUE"
+STMT_EXPR = "STMT_EXPR"
+
+
+class Stmt(object):
+    """docstring for Stmt."""
+    def __init__(self, kind):
+        self.kind = kind
+
+class StmtBlock(object):
+    """docstring for StmtBlock."""
+    def __init__(self, stmts):
+        self.stmts = stmts
+
+
+class StmtDecl(Stmt):
+    """docstring for StmtDecl."""
+    def __init__(self, decl):
+        super(StmtDecl, self).__init__(STMT_DECL)
+        self.decl = decl
+
+class ElseIf(object):
+    """docstring for ElseIf."""
+    def __init__(self, cond, block):
+        self.cond = cond
+        self.block = block
+
+class StmtIf(Stmt):
+    """docstring for StmtIf."""
+    def __init__(self, cond, then_block, elseifs, else_block):
+        super(StmtIf, self).__init__(STMT_IF)
+        self.cond = cond
+        self.then_block = then_block
+        self.elseifs = elseifs
+        self.else_block = else_block
+
+class StmtWhile(Stmt):
+    """docstring for StmtWhile."""
+    def __init__(self, expr, block):
+        super(StmtWhile, self).__init__(STMT_WHILE)
+        self.expr = expr
+        self.block = block
+
+class StmtFor(Stmt):
+    """docstring for StmtFor."""
+    def __init__(self, init, cond, update, block):
+        super(StmtFor, self).__init__(STMT_FOR)
+        self.init = init
+        self.cond = cond
+        self.update = update
+        self.block = block
+
+class StmtDoWhile(Stmt):
+    """docstring for StmtDoWhile."""
+    def __init__(self, block, expr):
+        super(StmtDoWhile, self).__init__(STMT_DO_WHILE)
+        self.block = block
+        self.expr = expr
+
+class StmtReturn(Stmt):
+    """docstring for StmtReturn."""
+    def __init__(self, expr):
+        super(StmtReturn, self).__init__(STMT_RETURN)
+        self.expr = expr
+
+class StmtBreak(Stmt):
+    """docstring for StmtBreak."""
+    def __init__(self):
+        super(StmtBreak, self).__init__(STMT_BREAK)
+
+class StmtContinue(Stmt):
+    """docstring for StmtContinue."""
+    def __init__(self):
+        super(StmtContinue, self).__init__(STMT_CONTINUE)
+
+class SwitchCase(object):
+    """docstring for SwitchCase."""
+    def __init__(self, exprs, stmts):
+        self.exprs = exprs
+        self.stmts = stmts
+
+class StmtSwitch(Stmt):
+    """docstring for StmtSwitch."""
+    def __init__(self, expr, cases):
+        super(StmtSwitch, self).__init__(STMT_SWITCH)
+        self.expr = expr
+        self.cases = cases
+
+class StmtExpr(Stmt):
+    """docstring for StmtExpr."""
+    def __init__(self, expr):
+        super(StmtExpr, self).__init__(STMT_EXPR)
+        self.expr = expr
+
+############################################################
+
+EXPR_TERNARY = "EXPR_TERNARY"
+EXPR_BINARY = "EXPR_BINARY"
+EXPR_UNARY = "EXPR_UNARY"
+EXPR_INT = "EXPR_INT"
+EXPR_FLOAT = "EXPR_FLOAT"
+EXPR_STR = "EXPR_STR"
+EXPR_NAME = "EXPR_NAME"
+EXPR_COMPOUND = "EXPR_COMPOUND"
+EXPR_CALL = "EXPR_CALL"
+EXPR_INDEX = "EXPR_INDEX"
+EXPR_FIELD = "EXPR_FIELD"
+EXPR_SIZEOF_EXPR = "EXPR_SIZEOF_EXPR"
+EXPR_SIZEOF_TYPE = "EXPR_SIZEOF_TYPE"
+
+
+class Expr(object):
+    """docstring for Expr."""
+    def __init__(self, kind):
+        self.kind = kind
+
+class ExprTernary(Expr):
+    """docstring for ExprTernary."""
+    def __init__(self, cond, then_expr, else_expr):
+        super(ExprTernary, self).__init__(EXPR_TERNARY)
+        self.cond = cond
+        self.then_expr = then_expr
+        self.else_expr = else_expr
+
+class ExprBinary(Expr):
+    """docstring for ExprBinary."""
+    def __init__(self, op, left, right):
+        super(ExprBinary, self).__init__(EXPR_BINARY)
+        self.op = op
+        self.left = left
+        self.right = right
+
+class ExprUnary(Expr):
+    """docstring for ExprUnary."""
+    def __init__(self, op, expr):
+        super(ExprUnary, self).__init__(EXPR_UNARY)
+        self.expr = expr
+
+class ExprInt(Expr):
+    """docstring for ExprInt."""
+    def __init__(self, val):
+        super(ExprInt, self).__init__(EXPR_INT)
+        self.val = val
+
+class ExprFloat(Expr):
+    """docstring for ExprFloat."""
+    def __init__(self, val):
+        super(ExprFloat, self).__init__(EXPR_FLOAT)
+        self.val = val
+
+class ExprStr(Expr):
+    """docstring for ExprStr."""
+    def __init__(self, val):
+        super(ExprStr, self).__init__(EXPR_STR)
+        self.val = val
+
+class ExprName(Expr):
+    """docstring for ExprName."""
+    def __init__(self, name):
+        super(ExprName, self).__init__(EXPR_NAME)
+        self.name = name
+
+class ExprCall(Expr):
+    """docstring for ExprCall."""
+    def __init__(self, operand, args):
+        super(ExprCall, self).__init__(EXPR_CALL)
+        self.operand = operand
+        self.args = args
+
+class ExprIndex(Expr):
+    """docstring for ExprIndex."""
+    def __init__(self, operand, expr):
+        super(ExprIndex, self).__init__(EXPR_INDEX)
+        self.operand = operand
+        self.expr = expr
+
+class ExprField(Expr):
+    """docstring for ExprField."""
+    def __init__(self, operand, field_name):
+        super(ExprField, self).__init__(EXPR_FIELD)
+        self.operand = operand
+        self.field_name = field_name
+
+class ExprCompound(Expr):
+    """docstring for ExprCompound."""
+    def __init__(self, type_, args):
+        super(ExprCompound, self).__init__(EXPR_COMPOUND)
+        self.type_ = type_
+        self.args = args
+
+class ExprSizeofExpr(Expr):
+    """docstring for ExprSizeofExpr."""
+    def __init__(self, expr):
+        super(ExprSizeofExpr, self).__init__(EXPR_SIZEOF_EXPR)
+        self.expr = expr
+
+class ExprSizeofType(Expr):
+    """docstring for ExprSizeofType."""
+    def __init__(self, type_):
+        super(ExprSizeofType, self).__init__(EXPR_SIZEOF_TYPE)
+        self.type_ = type_
--- src/phparser.py
+++ src/phparser.py
@@ -1,5 +1,6 @@
 import phlex
 from phtoken import *
+from phast import *
 
 
 def syntax_error(msg):
@@ -8,10 +9,7 @@ def syntax_error(msg):
 	print('\t' + phlex.source.splitlines()[phlex.line - 1])
 	exit(1)
 
-
 def is_token(t_type):
-	if type(t_type) is str:
-		return phlex.token.t_val == t_type
 	return phlex.token == t_type
 
 
@@ -21,71 +19,151 @@ def match_token(t_type):
 		return True
 	return False
 
+def is_keyword(keyword):
+	assert(type(keyword) is str)
+	return phlex.token == TOKEN_KEYWORD and phlex.token.t_val == keyword
+
+def match_keyword(keyword):
+	assert(type(keyword) is str)
+	if phlex.token == TOKEN_KEYWORD and phlex.token.t_val == keyword:
+		phlex.next_token()
+		return True
+	return False
 
 def expect_token(t_type):
-	if is_token(t_type):
-		phlex.next_token()
+	if not match_token(t_type):
+		syntax_error('Expected token \'%s\', got %s' % (STR_TOKEN_TYPE[t_type], str(phlex.token)))
+
+##############################################################################
+# LEFT OFF AT: Finished Declaration parsing, start statements, exprs and types
+##############################################################################
+
+def parse_name():
+	name = phlex.token.t_val
+	expect_token(TOKEN_NAME)
+	return name
+
+def parse_var_decl():
+	name = parse_name()
+	type_ = None
+	expr = None
+	if match_token(TOKEN_COLON):
+		type_ = parse_type()
+		if match_token(TOKEN_ASSIGN):
+			expr = parse_expr()
+	elif match_token(TOKEN_ASSIGN):
+		expr = parse_expr()
 	else:
-		expected = ''
-		if type(t_type) is str:
-			expected = t_type
-		else:
-			expected = STR_TOKEN_TYPE[t_type]
-		syntax_error('Expected token %s, got %s' % (expected, str(phlex.token)))
+		syntax_error("Expected ':' or '=' after var declaration, got %s" % str(phlex.token))
+	expect_token(TOKEN_SEMICOLON)
+	return DeclVar(name, type_, expr)
 
+def parse_const_decl():
+	name = parse_name()
+	expect_token(TOKEN_ASSIGN)
+	expr = parse_expr()
+	expect_token(TOKEN_SEMICOLON)
+	return DeclConst(name, expr)
 
-# literal: INT | FLOAT | '(' expr ')'
-# factor: [-] factor | literal
-# term: factor ([*/%] factor)*
-# expr: term ([+-] term)*
+def parse_typedef_decl():
+	name = parse_name()
+	expect_token(TOKEN_ASSIGN)
+	type_ = parse_type()
+	expect_token(TOKEN_SEMICOLON)
+	return DeclTypedef(name, type_)
 
-def parse_literal():
-	if is_token(TOKEN_INT):
-		int_val = phlex.token.t_val
-		phlex.next_token()
-		return int_val
-	elif is_token(TOKEN_FLOAT):
-		float_val = phlex.token.t_val
-		phlex.next_token()
-		return float_val
-	elif match_token('('):
-		ret_val = parse_expr()
-		expect_token(')')
-		return ret_val
+def parse_enum_item():
+	name = parse_name()
+	expr = None
+	if match_token(TOKEN_ASSIGN):
+		expr = parse_expr()
+	return EnumItem(name, expr)
+
+def parse_enum_decl():
+	name = parse_name()
+	expect_token(TOKEN_LBRACE)
+	items = []
+	if not is_token(TOKEN_RBRACE):
+		items.append(parse_enum_item())
+		while match_token(TOKEN_COMMA):
+			if is_token(TOKEN_RBRACE):
+				break
+			items.append(parse_enum_item())
+	expect_token(TOKEN_RBRACE)
+	return DeclEnum(name, items)
+
+def parse_aggregate_field():
+	if is_token(TOKEN_NAME):
+		name = parse_name()
+		expect_token(TOKEN_COLON)
+		type_ = parse_type()
+		return AggregateField(name, type_, None)
 	else:
-		syntax_error('Invalid token ' + str(phlex.token))
-
-def parse_factor():
-	if match_token('-'):
-		return -parse_factor()
-	return parse_literal()
-
-def parse_term():
-	lvalue = parse_factor()
-	while is_token('*') or is_token('/') or is_token('%'):
-		op = phlex.token.t_val
-		phlex.next_token()
-		rvalue = parse_factor()
-		if op == '*':
-			lvalue *= rvalue
-		elif op == '/':
-			if rvalue == 0:
-				# TODO: Implement an Exception/Error system for handling different exceptions
-				raise ZeroDivisionError
-			lvalue /= rvalue
+		if match_keyword('struct'):
+			return parse_struct_decl()
+		elif match_keyword('union'):
+			return parse_union_decl()
+		elif match_keyword('enum'):
+			return parse_enum_decl()
 		else:
-			lvalue %= rvalue
-	return lvalue
+			syntax_error("Invalid aggregate field '%s'" % str(phlex.token))
 
-def parse_expr():
-	lvalue = parse_term()
-	while is_token('+') or is_token('-'):
-		op = phlex.token.t_val
-		phlex.next_token()
-		rvalue = parse_term()
-		if op == '+':
-			lvalue += rvalue
-		else:
-			lvalue -= rvalue
+def parse_aggregate_fields():
+	expect_token(TOKEN_LBRACE)
+	fields = []
+	while not is_token(TOKEN_RBRACE):
+		fields.append(parse_aggregate_field())
+	expect_token(TOKEN_RBRACE)
+	return fields
 
-	return lvalue
+def parse_struct_decl():
+	name = parse_name()
+	fields = parse_aggregate_fields()
+	return DeclAggregate(DECL_STRUCT, name, fields)
+
+def parse_union_decl():
+	name = parse_name()
+	fields = parse_aggregate_fields()
+	return DeclAggregate(DECL_UNION, name, fields)
+
+def parse_func_arg():
+	name = parse_name()
+	expect_token(TOKEN_COLON)
+	type_ = parse_type()
+	return FuncArg(name, type_)
+
+def parse_func_decl():
+	name = parse_name()
+	expect_token(TOKEN_LPAREN)
+	func_args = []
+	if not is_token(TOKEN_RPAREN):
+		func_args.append(parse_func_arg())
+		while match_token(TOKEN_COMMA):
+			func_args.append(parse_func_arg())
+	expect_token(TOKEN_RPAREN)
+	ret_type = None
+	if match_token(TOKEN_ARROW):
+		ret_type = parse_type()
+	block = parse_stmt_block()
+	return DeclFunc(name, func_args, ret_type, block)
+
+def parse_context_decl():
+	if match_keyword('var'):
+		return parse_var_decl()
+	elif match_keyword('const'):
+		return parse_const_decl()
+	elif match_keyword('typedef'):
+		return parse_typedef_decl()
+	elif match_keyword('struct'):
+		return parse_struct_decl()
+	elif match_keyword('union'):
+		return parse_union_decl()
+	elif match_keyword('enum'):
+		return parse_enum_decl()
+	else:
+		syntax_error('Unknown declaration \'%s\'' % phlex.token.t_val)
+
+def parse_decl():
+	if match_keyword('function'):
+		return parse_func_decl()
+	return parse_context_decl()
